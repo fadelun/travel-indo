9a0dc67b888512dae7bc28e73e92897e760642fa
Muhammad Fadll <fadeljourney@gmail.com>
2023-02-16 23:06:44 +0700

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav id="nav-2" class="bg-transparent border-b-2 border-b-white text-white">
+    <div class="flex justify-between h-16 items-center px-16 md:px-24">
+      <div class="logo w-24">
+        <a href="#"> LOGO </a>
+      </div>
+      <div class="menu w-2/6 flex justify-between font-light">
+        <a href="#">Home</a><a href="#">About</a><a href="#">Tours</a
+        ><a href="#">Gallery</a>
+        <a href="#">Contacts</a>
+      </div>
+      <div class="button w-32 grid items-center">
+        <button>
+          <img src="@/assets/icons/search.svg" alt="search-icon" />
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+<style lang=""></style>
---
+++ src/views/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <Navbar />
+</template>
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "Header",
+  components: {
+    Navbar,
+  },
+};
+</script>
+<style lang=""></style>
