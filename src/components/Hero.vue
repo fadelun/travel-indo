75cbe9bb883ef5a7ba287600fd0dcdf254029aff
Muhammad Fadll <fadeljourney@gmail.com>
2023-02-17 22:42:58 +0700

    add hero section

---
+++ src/components/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="hero" class="bg-[#0000007e] h-full pt-20 relative">
+    <div class="container mx-auto">
+      <div class="title bg-yellow-200">
+        <h1 class="font-extrabold text-6xl text-white">
+          Indonesia destinations
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+<style lang=""></style>
