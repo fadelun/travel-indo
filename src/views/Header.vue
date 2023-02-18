a40384918005019154fba1b62bab1d80fb77a1c7
Muhammad Fadll <fadeljourney@gmail.com>
2023-02-18 14:52:04 +0700

    add main section

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,14 +1,18 @@
 <template>
-  <Navbar />
+  <header>
+    <Navbar />
+    <Hero />
+  </header>
 </template>
 <script>
 import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
 
 export default {
   name: "Header",
   components: {
     Navbar,
+    Hero,
   },
 };
 </script>
-<style lang=""></style>
