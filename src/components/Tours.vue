5469df8c85a0ce241b17f6e02ce88c17be4d3df6
Muhammad Fadll <fadeljourney@gmail.com>
2023-02-21 22:57:31 +0700

    add tours section

---
+++ src/components/Tours.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- popular tours -->
+  <div id="tours" class="content mb-12 h-[500px]">
+    <h3 class="font-bold text-4xl mx-auto w-fit mb-12">POPULAR TOURS</h3>
+
+    <!-- menu-cards -->
+    <ul class="menu-tour flex flex-wrap justify-between">
+      <li
+        v-for="tour in tours"
+        :key="tour"
+        class="card relative w-[260px] h-[340px] rounded-md border-4 border-white hover:scale-105 overflow-hidden"
+      >
+        <a href="#">
+          <div class="card-image h-full">
+            <img
+              :src="require(`@/assets/images/bdg/img1.jpg`)"
+              alt=""
+              srcset=""
+              class="object-center object-cover h-full"
+            />
+          </div>
+          <div
+            class="title text-center absolute inset-x-0 bottom-0 h-1/4 bg-[#0000006c]"
+          >
+            <h3 class="text-xl">{{ tour }}</h3>
+            <p class="text-sm">There will be small text</p>
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import { ref, onMounted } from "vue";
+import data from "../data/data.js";
+
+export default {
+  name: "Tours",
+
+  setup() {
+    const tours = ref(["tour 1", "tour 2", "tour 3", "tour 4"]);
+
+    onMounted(() => {
+      console.log(data);
+    });
+    return {
+      tours,
+    };
+  },
+};
+</script>
+<style></style>
