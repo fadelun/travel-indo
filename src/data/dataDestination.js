1290c6c9f1f0f3f549a3ce1b01c301c566a3b23c
Muhammad Fadll <fadeljourney@gmail.com>
2023-03-06 18:55:54 +0700

    add data and configured tailwind

---
+++ src/data/dataDestination.js
@@ -0,0 +1,13 @@
+import bromo1 from "../assets/images/mlg/img1.jpg"
+
+
+const dataDestination = [
+    {
+        id: 1,
+        name: "Bromo",
+        img: [bromo1],
+        desc: "Gunung Bromo atau dalam bahasa Tengger dieja " / Brama / ", juga disebut Kaldera Tengger, adalah sebuah gunung berapi aktif di Jawa Timur, Indonesia. Gunung ini memiliki ketinggian 2.329 meter di atas permukaan laut dan berada dalam empat wilayah kabupaten, yakni Kabupaten Probolinggo, Kabupaten Pasuruan, Kabupaten Lumajang, dan Kabupaten Malang. Gunung Bromo terkenal sebagai objek wisata utama di Jawa Timur. Sebagai sebuah objek wisata, Bromo menjadi menarik karena statusnya sebagai gunung berapi yang masih aktif. Gunung Bromo termasuk dalam kawasan Taman Nasional Bromo Tengger Semeru."
+    }
+]
+
+export default dataDestination;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // add new color
+        "dark-blue": "#030f33"
+
+      }
+    },
   },
   variants: {
     extend: {},
