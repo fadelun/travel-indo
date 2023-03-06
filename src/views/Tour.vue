271a8cacc2bc44dbc00ebcd39ea332b01012703b
Muhammad Fadll <fadeljourney@gmail.com>
2023-03-06 21:07:37 +0700

    updated tour section

--- src/views/Tour.vue
+++ src/views/Tour.vue
@@ -1,59 +1,71 @@
 <template>
-  <header class="h-[340px] bg-dark-blue">
+  <header class="bg-dark-blue">
     <Navbar />
+    <div class="hero h-[94vh] relative pt-20 container">
+      <figure class="absolute inset-0">
+        <img
+          src="../assets/images/mlg/img1.jpg"
+          class="w-full h-full object-cover"
+          alt="bromo"
+        />
+      </figure>
+    </div>
   </header>
-  <main class="bg-white">
-    <article
-      class="container bg-white text-slate-500 border-2 border-slate-200 rounded-md absolute -top-40 left-0 right-0 p-8"
-    >
-      <header class="mb-8">
-        <h2 class="text-dark-blue">Bromo</h2>
-        <p class="text-xl">Malang</p>
-        <figure>
-          <img
-            class="w-full h-[400px] object-cover object-center mt-5"
-            src="../assets/images/mlg/img1.jpg"
-            alt="boromo, malang"
-          />
-        </figure>
-      </header>
-      <section class="text">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-          natus possimus enim nesciunt beatae architecto dolorem deleniti
-          nostrum explicabo nemo asperiores, a alias voluptas cupiditate id amet
-          sed illo optio.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-          natus possimus enim nesciunt beatae architecto dolorem deleniti
-          nostrum explicabo nemo asperiores, a alias voluptas cupiditate id amet
-          sed illo optio Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Ut enim labore eos libero ipsum nisi similique, sit nesciunt
-          dicta, omnis, quas animi vitae iure iste est. Fugit maiores culpa
-          aliquam, blanditiis natus est aliquid, illo ipsum quasi libero aut
-          voluptatem consequatur, magni suscipit temporibus quis quam? Officia
-          molestias architecto debitis ab voluptatem ex consequuntur ducimus
-          deleniti est, illo quisquam necessitatibus assumenda nulla accusantium
-          praesentium? Tempore rerum rem recusandae odio ullam fuga ad aliquid
-          velit ducimus sed libero, doloremque suscipit dolore? Rem ex, possimus
-          eligendi, at cum accusamus earum perspiciatis libero odit nihil sunt,
-          praesentium adipisci molestiae vel assumenda iure quam?
-        </p>
-      </section>
-    </article>
+  <main class="bg-white pt-6">
+    <div class="container flex">
+      <div class="content w-3/4">
+        <article class="">
+          <div class="header mb-4">
+            <h2 class="text-dark-blue">Bromo</h2>
+            <p class="text-xl text-slate-600">Malang</p>
+          </div>
+          <div class="main text-slate-600">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
+              sapiente illo similique commodi fuga vitae alias, in doloremque,
+              cumque temporibus laboriosam exercitationem! Dicta incidunt, a
+              animi iusto fuga ex itaque voluptate obcaecati eius sed quos harum
+              magni magnam debitis iure cumque dolores sapiente temporibus
+              repellendus laborum pariatur veniam. Esse consectetur totam
+              debitis non quidem iste officia facere at pariatur optio?
+              Doloremque, eaque itaque quas ipsa labore corporis similique est
+              officia repellat officiis natus illo? Doloremque itaque dicta
+              voluptatum accusamus nihil provident eum natus corrupti ipsum ea,
+              incidunt veniam nulla! Doloremque harum, reiciendis atque labore
+              porro vel! Ex ducimus nihil vero magnam fugiat numquam quibusdam,
+              perspiciatis recusandae est molestias cumque inventore in hic ea
+              praesentium deleniti. Consectetur corrupti expedita aliquam illo
+              harum at esse et, accusantium quo deleniti exercitationem
+              perspiciatis excepturi laboriosam dolorem? Obcaecati, natus hic
+              vitae eum, ipsam quasi molestias totam quas corrupti rem fuga
+              facere quis possimus tempore. Itaque soluta incidunt provident
+              laudantium inventore deleniti maiores, tenetur ex amet, nihil
+              tempora quam molestias esse corrupti repellat harum sit odit
+              delectus explicabo quibusdam? Iure magnam laboriosam eos
+              repudiandae quibusdam sapiente quisquam velit inventore ducimus
+              non praesentium reprehenderit incidunt, quas labore et possimus
+              officiis nihil perferendis debitis quod minus corporis minima.
+            </p>
+          </div>
+        </article>
+      </div>
+      <aside class="bg-purple-600 w-1/4">
+        Gallery of images
+        <div class="gallery-images">...</div>
+      </aside>
+    </div>
   </main>
-  <Footer />
+  <!-- <Footer /> -->
 </template>
 <script>
 import Navbar from "@/components/Navbar.vue";
-import Footer from "@/components/Footer.vue";
+// import Footer from "@/components/Footer.vue";
 
 export default {
   name: "Tour",
   components: {
     Navbar,
-    Footer,
+    // Footer,
   },
 };
 </script>
