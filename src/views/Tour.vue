13c9da85404816e69df863e8b9c4776ddfb03a36
Muhammad Fadll <fadeljourney@gmail.com>
2023-03-06 16:38:41 +0700

    compress images

--- src/views/Tour.vue
+++ src/views/Tour.vue
@@ -2,19 +2,17 @@
   <!-- <div class="header bg-black h-24">
     <Navbar />
   </div> -->
-  <header class="h-[92vh]">
+  <header class="h-[500px] bg-[#030f33]">
     <Navbar />
-    <div class="hero h-full relative pt-20 container">
-      <div class="wrapper-title absolute left-2 bottom-2 text-white">
+    <!-- <div class="hero h-full relative pt-20 container">
+      <div class="wrapper-title absolute left-2 bottom-5 text-white">
         <h3 class="title">Bromo, Malang</h3>
       </div>
-    </div>
+    </div> -->
   </header>
   <div class="main"></div>
 </template>
 <script>
-// import Header from "@/components/Header.vue";
-
 import Navbar from "@/components/Navbar.vue";
 
 export default {
